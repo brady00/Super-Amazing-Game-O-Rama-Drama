0706516b37993251ae18ef79227fb504748eabae
brady <guyer.brady@gmail.com>
2016-10-06 20:07:08 -0400

    initial commit of base files

---
+++ MonkeyEngine/MonkeyEngine/Renderer/Managers/IndexBufferManager.cpp
@@ -0,0 +1,12 @@
+#include "IndexBufferManager.h"
+
+
+
+IndexBufferManager::IndexBufferManager()
+{
+}
+
+
+IndexBufferManager::~IndexBufferManager()
+{
+}
---
+++ MonkeyEngine/MonkeyEngine/Renderer/Managers/IndexBufferManager.h
@@ -0,0 +1,8 @@
+#pragma once
+class IndexBufferManager
+{
+public:
+	IndexBufferManager();
+	~IndexBufferManager();
+};
+
